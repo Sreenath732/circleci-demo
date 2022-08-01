5fb545cbdffebf2324c573fabb8c7444ad19f832
Sreenath732 <110103544+Sreenath732@users.noreply.github.com>
2022-08-01 14:51:29 +0530

    testing

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def Add(a, b):
         return a + b
         
 def SayHello():
-        print("sup world from srcmake sreenath")
+        print("sup world from srcmake sreenath test")
 
 if __name__ == '__main__':
         SayHello()
